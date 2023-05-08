5cf2080bb8ea68acd4beffafd5f131787bdd51cd
Zachary Fouant <zacktheattack45@gmail.com>
2023-05-08 17:16:06 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# **_PacMan Factory_** #
+
+The button on the left spawns a PacMan in a random spot of the screen, the button on the right starts movement of all current and future PacMen. When colliding with the edge of the screen, the PacMens' horizontal movement will reverse, and likewise with the tops and bottoms in relation to its verticality.
+
+# Installation
+
+### Steps to install ###
+
+1. Click the dropdown arrow on the green "<> Code" button on the top right, next to the About section.
+2. Select download as zip.
+3. Extract files into directory of choice.
+4. The files should now display full functionality within the html file. Enjoy!
+
+# Future Implementations Roadmap #
+
+- v2.0 Add code to randomly change color upon instantiation and upon striking a border.
+- v3.0 Add sound and (mutable) music
+
+# License #
+
+Copyright 2023 Zachary B. Fouant
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ pacmen.js
@@ -0,0 +1,75 @@
+const pacArray = [
+  ['./images/PacMan1.png', './images/PacMan2.png'],
+  ['./images/PacMan3.png', './images/PacMan4.png'],
+];
+let direction = 0;
+const pacMen = []; // Array to hold PacMen
+
+// Returns object with random values for use in velocity and positioning
+function setToRandom(scale) {
+  return {
+    x: Math.random() * scale,
+    y: Math.random() * scale,
+  };
+}
+
+// Factory to make a PacMan at a random position with random velocity
+function makeNewPac() {
+  // returns an object with random values scaled {x: 33, y: 21}
+  let velocity = setToRandom(10);
+  let position = setToRandom(200);
+
+  // Add image to div id = game
+  let game = document.getElementById('game');
+  let newimg = document.createElement('img');
+  newimg.style.position = 'absolute';
+  newimg.src = './images/PacMan1.png';
+  newimg.width = 100;
+
+  //sets left and top attributes for newImg to value of x, y positions
+  newimg.style.left = position.x
+  newimg.style.top = position.y
+
+  // add newImg to game
+  game.appendChild(newimg);
+
+  // return object details
+  return {
+    position,
+    velocity,
+    newimg,
+  };
+}
+
+  // loop through spawned pacmen, and move them around, bouncing away from walls when collision is detected.
+function update() {
+  pacMen.forEach((item) => {
+    checkCollisions(item);
+    item.position.x += item.velocity.x;
+    item.position.y += item.velocity.y;
+
+    item.newimg.style.left = item.position.x;
+    item.newimg.style.top = item.position.y;
+  });
+  setTimeout(update, 20);
+}
+
+function checkCollisions(item) {
+  if (item.position.x + item.velocity.x + item.newimg.width > window.innerWidth || item.position.x + item.velocity.x < 0) {
+    item.velocity.x = -item.velocity.x; // reverse x direction
+    if (item.velocity.x > 0) {
+      // PacMan is moving to the right
+      item.newimg.src = pacArray[0][0];
+    } else {
+      // PacMan is moving to the left
+      item.newimg.src = pacArray[1][0];
+    }
+  }
+  if (item.position.y + item.velocity.y + item.newimg.height > window.innerHeight || item.position.y + item.velocity.y < 0) {
+    item.velocity.y = -item.velocity.y; // reverse y direction
+  }
+}
+
+function makeOne() {
+  pacMen.push(makeNewPac()); // add a new PacMan
+}
